09111178ec6bc2747b69c3153ea2554f30804010
shlomo-shamy <gatefleet@gmail.com>
2025-12-06 23:39:58 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ console.log('Memory usage:', process.memoryUsage());
 console.log('Environment variables set:', Object.keys(process.env).length);
 console.log('PORT from environment:', process.env.PORT);
 console.log('Current working directory:', process.cwd());
-
+ 
 // Catch any unhandled errors
 process.on('uncaughtException', (error) => {
   console.error('UNCAUGHT EXCEPTION:', error);
